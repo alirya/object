9696b53b6e15ae1c700d583ad91130e3d86a0c2d
dikac <rigel234@yahoo.com>
2021-12-07 20:01:00 +0700

    remove namespace module

--- src/validator/validatable/record/value-partial.ts
+++
@@ -1,9 +0,0 @@
-import ValuePartialParameters from "./value-partial-parameters";
-import ValuePartialParameter from "./value-partial-parameter";
-
-namespace ValuePartial {
-
-    export const Parameter = ValuePartialParameter;
-    export const Parameters = ValuePartialParameters;
-}
-export default ValuePartial;
--- src/validator/value-all.ts
+++
@@ -1,9 +0,0 @@
-import ValueAllParameters from "./value-all-parameters";
-import ValueAllParameter from "./value-all-parameter";
-
-namespace ValueAll {
-
-    export const Parameters = ValueAllParameters;
-    export const Parameter = ValueAllParameter;
-}
-export default ValueAll;
