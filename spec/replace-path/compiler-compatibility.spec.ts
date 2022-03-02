4a52f77cc7d4978589d9d8c56c8442a3d7412706
dikac <rigel234@yahoo.com>
2022-03-02 23:05:35 +0700

    add replace path

---
+++ spec/replace-path/compiler-compatibility.spec.ts
@@ -0,0 +1,27 @@
+import ReplacePath from "../../src/replace-path";
+
+
+it('enable console log', () => spyOn(console, 'log').and.callThrough());
+
+
+it('test', () =>{
+
+    const source = {
+        parent : {
+            child : {
+                number : 1
+            }
+        }
+    }
+
+
+    const updated : ReplacePath<typeof source, string, ['parent', 'child', 'number']> = {
+        parent : {
+            child : {
+                number : '1'
+            }
+        }
+    }
+
+
+})
---
+++ src/replace-path.ts
@@ -0,0 +1,12 @@
+import {Object} from "ts-toolbelt";
+
+/**
+ * Replace {@template Object} value with {@template Replace} at {@template Property}
+ */
+type ReplacePath<
+    Target extends object,
+    Replace extends any,
+    Properties extends PropertyKey[],
+> = Object.P.Omit<Target, Properties> & Object.P.Record<Properties, Replace>;
+
+export default ReplacePath;
