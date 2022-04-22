4bd19c9cb18f334dd2de5e6a3ccf9141e9e48b17
dikac <rigel234@yahoo.com>
2022-04-22 17:14:08 +0700

    improve spec

--- spec/property/boolean/exists-deep/standard.spec.ts
+++ spec/property/boolean/exists-deep/standard.spec.ts
@@ -23,9 +23,12 @@ describe(`parameters`, () => {
 
         expect(ExistsDeepParameters(object, 'a', )).toBe(true);
         expect(ExistsDeepParameters(object, 'b', )).toBe(true);
-        expect(ExistsDeepParameters(object, 'c', 'c', 'c')).toBe(true);
+        expect(ExistsDeepParameters(object, 'c', )).toBe(true);
+
         expect(ExistsDeepParameters(object, 'b', 'a')).toBe(true);
         expect(ExistsDeepParameters(object, 'b', 'b')).toBe(true);
+        expect(ExistsDeepParameters(object, 'c', 'c', 'c')).toBe(true);
+        expect(ExistsDeepParameters(object, 'c', 'c')).toBe(true);
 
     });
 
@@ -33,6 +36,7 @@ describe(`parameters`, () => {
 
         expect(ExistsDeepParameters(object, 'a', 'b')).toBe(false);
         expect(ExistsDeepParameters(object, 'a', 'a')).toBe(false);
+        expect(ExistsDeepParameters(object, 'c', 'a')).toBe(false);
     });
 });
 
@@ -42,9 +46,12 @@ describe(`parameters`, () => {
 
         expect(ExistsDeepParameter({object, properties:['a']})).toBe(true);
         expect(ExistsDeepParameter({object, properties:['b']})).toBe(true);
-        expect(ExistsDeepParameter({object, properties:['c', 'c', 'c']})).toBe(true);
+        expect(ExistsDeepParameter({object, properties:['c']})).toBe(true);
+
         expect(ExistsDeepParameter({object, properties:['b', 'a']})).toBe(true);
         expect(ExistsDeepParameter({object, properties:['b', 'b']})).toBe(true);
+        expect(ExistsDeepParameter({object, properties:['c', 'c', 'c']})).toBe(true);
+        expect(ExistsDeepParameter({object, properties:['c', 'c']})).toBe(true);
 
 
     });
@@ -53,6 +60,7 @@ describe(`parameters`, () => {
 
         expect(ExistsDeepParameter({object, properties:['a', 'b']})).toBe(false);
         expect(ExistsDeepParameter({object, properties:['a', 'a']})).toBe(false);
+        expect(ExistsDeepParameter({object, properties:['c', 'a']})).toBe(false);
 
     });
 });
