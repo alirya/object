9889219ab375355af238d273fa93a9d4bdfbb039
dikac <rigel234@yahoo.com>
2022-08-07 18:54:28 +0700

    feat: add omti null and undefined type

--- src/omit-undefined.ts
+++ src/omit-undefined.ts
@@ -1,12 +1,15 @@
 import {Object} from 'ts-toolbelt';
 
+
+export type OmitUndefinedType<O extends object> = Omit<O, keyof Object.Select<O, undefined>>;
+
 /**
  * remove undefined from {@param object}
  * @param object
  */
 export default function OmitUndefined<O extends object>(
     object : O,
-)  : Omit<O, keyof Object.Select<O, undefined>> {
+)  : OmitUndefinedType<O> {
 
     for(let property in object) {
 
