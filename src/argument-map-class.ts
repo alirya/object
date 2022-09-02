73a648e35a7bc3b2b85a4b668ba4061469e05681
dikac <rigel234@yahoo.com>
2022-09-02 22:14:41 +0700

    ref: improve type

--- src/argument-map-class.ts
+++ src/argument-map-class.ts
@@ -3,7 +3,7 @@
  * map record of parameter to record of function with the same parameter
  */
 type ArgumentMapClass<
-    Arguments extends Record<PropertyKey, unknown>,
+    Arguments extends object,
     Returns extends Record<keyof Arguments, any> = Record<keyof Arguments, any>,
 > = {
     [Key in keyof Arguments] : (arg:Arguments[Key])=>Returns[Key]
