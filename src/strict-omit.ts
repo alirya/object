b930a5b8fbf595c2ee683e13584307748b5c55b8
dikac <rigel234@yahoo.com>
2023-12-27 10:41:48 +0700

    ref: improve strict omit

--- src/strict-omit.ts
+++ src/strict-omit.ts
@@ -1,8 +1,10 @@
+import {OmitStrict} from './omit';
+
 /**
  * @deprecated
  * Strict Omit
  * native, {@package utility-types}, {@package ts-toolbelt} does not provide strict
  */
-type StrictOmit<Object extends object, Key extends keyof  Object> = globalThis.Omit<Object, Key>;
+type StrictOmit<Object extends object, Key extends keyof  Object> = OmitStrict<Object, Key>;
 
 export default StrictOmit;
